b91d14102d4726ee0250965451b505cff4abb8c0
boygioykaskyriakos <boygioykaskyriakos@gmail.com>
2020-08-02 22:44:21 +0300

    critical results visualisation

--- find_outliers_platform/outlier_algorithms/base_class_outlier_algorithms.py
+++ find_outliers_platform/outlier_algorithms/base_class_outlier_algorithms.py
@@ -11,7 +11,9 @@ from static_files.standard_variable_names import DATA_TYPE, NODE, KEY, \
 class BaseClassOutlierAlgorithms(BaseClassAnalytic):
     OUTPUT_COLUMNS = [OUTLIER_NO, SUBSET_SIZE, SUBSET, NODE, DATA_TYPE, INDEX_FIRST_ELEMENT, INDEX_LAST_ELEMENT]
     OUTPUT_COLUMNS_METRICS = [SUBSET_SIZE, OUTLIER_NO]
-    OUTPUT_COLUMNS_METRICS_CRITICAL = [SUBSET_SIZE, TOTAL_PANICS]
+    OUTPUT_COLUMNS_METRICS_CRITICAL = [
+        SUBSET_SIZE, NODE, DATA_TYPE, INDEX_FIRST_ELEMENT, INDEX_LAST_ELEMENT, TOTAL_PANICS, SUBSET
+    ]
 
     def __init__(self):
         """
@@ -68,13 +70,28 @@ class BaseClassOutlierAlgorithms(BaseClassAnalytic):
         self.logger.info(msg)
 
     @staticmethod
-    def format_metrics_critical(df: pd.DataFrame, critical_value: float) -> pd.DataFrame:
-        df = df.groupby(
+    def format_metrics_critical(df_all_data: pd.DataFrame, critical_value: float) -> pd.DataFrame:
+        df = df_all_data.groupby(
             [SUBSET_SIZE, NODE, INDEX_FIRST_ELEMENT, INDEX_LAST_ELEMENT]
         ).count().reset_index()
 
+        df = pd.merge(
+            df[[SUBSET_SIZE, NODE, INDEX_FIRST_ELEMENT, INDEX_LAST_ELEMENT, OUTLIER_NO]],
+            df_all_data[[SUBSET_SIZE, NODE, DATA_TYPE, INDEX_FIRST_ELEMENT, INDEX_LAST_ELEMENT, SUBSET]],
+            on=[SUBSET_SIZE, NODE, INDEX_FIRST_ELEMENT, INDEX_LAST_ELEMENT],
+            how='left'
+        )
+
         df = df[df[OUTLIER_NO] > critical_value]
-        df = df.groupby(SUBSET_SIZE)[OUTLIER_NO].sum().reset_index()
+        df_per_node = df.groupby([SUBSET_SIZE, NODE])[OUTLIER_NO].sum().reset_index()
+
+        df = pd.merge(
+            df[[SUBSET_SIZE, NODE, DATA_TYPE, INDEX_FIRST_ELEMENT, INDEX_LAST_ELEMENT, SUBSET]],
+            df_per_node,
+            on=[SUBSET_SIZE, NODE],
+            how='left'
+        )
+
         df[OUTLIER_NO] = df[OUTLIER_NO] / 2
         df[OUTLIER_NO] = df[OUTLIER_NO].astype(int)
         df = df.rename(columns={OUTLIER_NO: TOTAL_PANICS})
