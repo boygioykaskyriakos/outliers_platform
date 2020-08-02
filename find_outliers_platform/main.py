8a347e12e81db7a3b57bd1c3dbec42808a8f3861
boygioykaskyriakos <boygioykaskyriakos@gmail.com>
2020-08-02 14:07:21 +0300

    rename folder of outlier algorithms

--- find_outliers_platform/main.py
+++ find_outliers_platform/main.py
@@ -1,7 +1,7 @@
 from static_files.dixon_q_test_confidence_data import STATIC_Q95, STATIC_Q90, STATIC_Q99
 from utilities.read_csv_to_pd_dataframe import ReadCSVToDataFrame
 from static_files.standard_variable_names import NODE, DATA_TYPE
-from dixon_q_test.dixon_q_test_class import FindOutlierDixon
+from outlier_algorithms.dixon_q_test_class import FindOutlierDixon
 
 if __name__ == "__main__":
     df_data = ReadCSVToDataFrame(field_name="DATA_SET_INFO", file_path="full_file_path", delimiter="delimiter").run()
