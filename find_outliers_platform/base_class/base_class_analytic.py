6bc5ce8b263718aa19de39da8c4e469aa4389446
boygioykaskyriakos <boygioykaskyriakos@gmail.com>
2020-08-15 21:33:29 +0300

    base class comments

--- find_outliers_platform/base_class/base_class_analytic.py
+++ find_outliers_platform/base_class/base_class_analytic.py
@@ -8,6 +8,10 @@ from static_files.standard_variable_names import GLOBALS, OUTPUT_PATH, PRINT_DEB
 
 
 class BaseClassAnalytic(object):
+    """
+    This is the base class of all the analytics
+    It implements class variables common for all instances and common to all derived classes
+    """
     read_ini_file_obj = ReadIniFile()
     save_file = SavePdToCSVFile(read_ini_file_obj.get_str(GLOBALS, OUTPUT_PATH))
     print_debug = read_ini_file_obj.get_boolean(GLOBALS, PRINT_DEBUG)
